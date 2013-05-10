aeb200df66e8ca37a62f616bb1990cf53206f433
Camilo Aguilar <camilo@acklenavenue.com>
2013-05-10 17:14:07 -0600

    Add fluent security

---
+++ MiniAmazon.Web/Infrastructure/FluentSecurityConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using FluentSecurity;
+using MiniAmazon.Web.Controllers;
+
+namespace MiniAmazon.Web.Infrastructure
+{
+    public static class FluentSecurityConfig
+    {
+        public static void Configure()
+        {
+            SecurityConfigurator.Configure(configuration =>
+            {
+                configuration.GetAuthenticationStatusFrom(() => HttpContext.Current.User.Identity.IsAuthenticated);
+
+                configuration.ForAllControllers().DenyAnonymousAccess();
+                configuration.For<AccountController>(x => x.SignIn()).Ignore();
+
+                //configuration.IgnoreMissingConfiguration(); Para que no devuelva exceptiones.
+
+                configuration.ResolveServicesUsing(type =>
+                {
+                    if (type == typeof(IPolicyViolationHandler))
+                    {
+                        var types = Assembly
+                            .GetAssembly(typeof(MvcApplication))
+                            .GetTypes()
+                            .Where(x => typeof(IPolicyViolationHandler).IsAssignableFrom(x)).ToList();
+
+                        var handlers = types.Select(t => Activator.CreateInstance(t) as IPolicyViolationHandler).ToList();
+
+                        return handlers;
+                    }
+                    return Enumerable.Empty<object>();
+                });
+            });
+
+        }
+    }
+}
