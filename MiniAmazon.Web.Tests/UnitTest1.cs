ea14fa508cfde323d9b2b8a5a07e42afe89f4394
Camilo Aguilar <camilo@acklenavenue.com>
2013-05-10 20:44:12 -0600

    Add unit test for web

---
+++ MiniAmazon.Web.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniAmazon.Domain;
+using MiniAmazon.Web.Controllers;
+using Moq;
+
+namespace MiniAmazon.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WhenCreatingNewInstanceForAccountController()
+        {
+            var mockRepository = new Mock<IRepository>();
+            var mockMappingEngine = new Mock<IMappingEngine>();
+            var accountController = new AccountController(mockRepository.Object, mockMappingEngine.Object);
+
+            Assert.IsNotNull(accountController);
+        }
+    }
+}
