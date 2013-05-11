a9338dcb51a758b9aedcbf3b7893738de4e53191
Camilo Aguilar <camilo@acklenavenue.com>
2013-05-11 15:21:24 -0600

    Fixed connection string and solution file

--- MiniAmazon.Web/Global.asax.cs
+++ MiniAmazon.Web/Global.asax.cs
@@ -58,7 +58,7 @@ namespace MiniAmazon.Web
         public static ISessionFactory CreateSessionFactory()
         {
             MsSqlConfiguration databaseConfiguration = MsSqlConfiguration.MsSql2008.ShowSql().
-                ConnectionString(x => x.FromConnectionStringWithKey("MiniAmazon.Local"));
+                ConnectionString(x => x.FromConnectionStringWithKey("MiniAmazon.Remote"));
             ISessionFactory sessionFactory = new SessionFactoryBuilder(new MappingScheme(), databaseConfiguration)
                 .Build();
 
